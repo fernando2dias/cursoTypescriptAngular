c4b9a2b6bd4e10620eb26cc8970309cf60634302
Fernando Dias <Fernando.Dias@flex.com>
2019-07-04 17:13:07 -0300

    novos exercicios

--- angular/typescript/dist/app2.js
+++ angular/typescript/dist/app2.js
@@ -31,5 +31,26 @@ var Concessionaria = /** @class */ (function () {
     };
     return Concessionaria;
 }());
-var concessionaria = new Concessionaria('Av Paulista');
-console.log(concessionaria);
+var Pessoa = /** @class */ (function () {
+    function Pessoa(nome, carroPreferido) {
+        this.nome = "";
+        this.carroPreferido = "";
+        this.nome = nome;
+        this.carroPreferido = carroPreferido;
+    }
+    Pessoa.prototype.dizerNome = function () {
+        return this.nome;
+    };
+    Pessoa.prototype.dizerCarroPreferido = function () {
+        return this.carroPreferido;
+    };
+    Pessoa.prototype.comprarCarro = function (carro) {
+        this.carro = carro;
+    };
+    Pessoa.prototype.dizerCarroQueTem = function () {
+        return this.carro;
+    };
+    return Pessoa;
+}());
+var pessoa = new Pessoa("Fernando", "Golf");
+console.log(pessoa.dizerNome() + " " + pessoa.dizerCarroPreferido());
